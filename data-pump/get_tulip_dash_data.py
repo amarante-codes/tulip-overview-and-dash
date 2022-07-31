5ffe004ba18447940f3dcfe2aad7f59ff5d15747
amarante-codes <amar@mrnt.xyz>
2022-07-31 15:26:54 -0500

    data pump feeds to cloud storage bucket #25

--- data-pump/get_tulip_dash_data.py
+++ data-pump/get_tulip_dash_data.py
@@ -17,6 +17,28 @@ import json
 
 from google.cloud import storage
 
+def upload_blob_from_memory(bucket_name, destination_blob_name, contents):
+    """Uploads a file to the bucket."""
+
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The contents to upload to the file
+    # contents = "these are my contents"
+
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_string(contents)
+
+    print(
+        f"{destination_blob_name} with contents {contents} uploaded to {bucket_name}."
+    )
+
 # open the pipelines file
 pipelines = {}
 with open('pipelines.json', 'r') as pl_file:
@@ -32,3 +54,4 @@ for obj in pipelines['pipelines']:
     # store the results
     print(f'The pipelines object is: {obj}')
     print(f'The result is: {result.json()}')
+    upload_blob_from_memory(target_bucket, obj['targetObject'], result.json())
